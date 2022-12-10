0854bf74373004547657d244c3ddcb843da2a0c6
Brijesh Bhatt <brjsh.leo@gmail.com>
2022-12-10 13:09:14 +0530

    added more

---
+++ LeetCode/src/com/leetcode/solutions/medium/WordBreak.java
@@ -0,0 +1,68 @@
+package com.leetcode.solutions.medium;
+
+import java.util.*;
+
+/**
+ * Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated
+ * sequence of one or more dictionary words.
+ *
+ * Note that the same word in the dictionary may be reused multiple times in the segmentation.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "leetcode", wordDict = ["leet","code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet code".
+ * Example 2:
+ *
+ * Input: s = "applepenapple", wordDict = ["apple","pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+ * Note that you are allowed to reuse a dictionary word.
+ * Example 3:
+ *
+ * Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 300
+ * 1 <= wordDict.length <= 1000
+ * 1 <= wordDict[i].length <= 20
+ * s and wordDict[i] consist of only lowercase English letters.
+ * All the strings of wordDict are unique.
+ */
+public class WordBreak {
+
+    public static void main(String[] args) {
+        //System.out.println(wordBreak("leetcode", Arrays.asList("leet", "code")));
+        System.out.println(wordBreak("applepenapple", Arrays.asList("apple","pen")));
+        //System.out.println(wordBreak("catsandog", Arrays.asList("cats","dog","sand","and","cat")));
+    }
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[s.length()];
+        queue.add(0);
+        while (!queue.isEmpty()) {
+            int start = queue.remove();
+            if (visited[start]) {
+                continue;
+            }
+            for (int end = start + 1; end <= s.length(); end++) {
+                if (wordDictSet.contains(s.substring(start, end))) {
+                    queue.add(end);
+                    if (end == s.length()) {
+                        return true;
+                    }
+                }
+            }
+            visited[start] = true;
+        }
+        return false;
+    }
+}
--- LeetCode/src/com/leetcode/solutions/others/PowerSet.java
+++ LeetCode/src/com/leetcode/solutions/others/PowerSet.java
@@ -1,31 +1,40 @@
 package com.leetcode.solutions.others;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PowerSet {
 
-    static void printPowerSet(char[] set, int setSize) {
+    public static Set<String> powerSet(char[] set, int setSize) {
 
         /* setSize of power set of a set with setSize n is (2**n -1) */
         long powSetSize = (long) Math.pow(2, setSize);
 
         int counter, j;
 
+        Set<String> output = new HashSet<>();
+
         /*Run from counter 000..0 to 111..1*/
         for(counter = 0; counter < powSetSize; counter++) {
+            String str = "";
             for(j = 0; j < setSize; j++) {
                 /* Check if jth bit in the counter is set If set then print jth element from set */
                 if((counter & (1 << j)) > 0) {
-                    System.out.print(" counter=" + counter + ", j=" + j + " ");
-                    System.out.print(set[j]);
+                    //System.out.print(" counter=" + counter + ", j=" + j + " ");
+                    //System.out.print(set[j]);
+                    str = str.concat(String.valueOf(set[j]));
+                    output.add(str);
                 }
             }
-            System.out.println();
+            //System.out.println();
         }
+        return output;
     }
 
     // Driver program to test printPowerSet
     public static void main (String[] args)
     {
         char[] set = {'a', 'b', 'c'};
-        printPowerSet(set, 3);
+        System.out.println(powerSet(set, 3));
     }
 }
